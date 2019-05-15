5c758047369262574869bc1dc82f66ac553bd2a2
Ian Lum <ianlum314@gmail.comm>
2019-05-15 13:56:22 -0700

    Akshar modifications to RSA

--- rsaThicc.py
+++ rsaThicc.py
@@ -1,40 +1,35 @@
 from primeStuff import relPrime, isPrime
 import random
 import time
+from modInv import modinv
+import math
 
-# initPrimes = [7753, 7901]
-# initPrimes = [1153, 1217]
-initPrimes = []
+
+initPrimes = [9013287591422353,20319995244432029]
 
 start = time.time()
-
-while len(initPrimes) < 2:
-    b = random.randint(1000,8000)
-    if isPrime(b):
-        initPrimes.append(b)
 
 p,q = initPrimes
 
 b = (p-1)*(q-1)
 
 n = p*q
-e = random.choice(relPrime(b)) # rel prime to b
+e = 65537 # rel prime to b
 
-d = 1# e mod b = 1
-while (d*e)%b != 1:
-    d += 1
+d = modinv(e,b) # d * e mod b = 1
 
 stop = time.time()
 
-# m = random.randint(1,n) # anything < n
-
-# mhat= (m**e)%n
-
-# decode = (mhat**d)%n
+m = random.randint(1,n) # anything < n
+mhat= pow(m,e,n)
+decode = pow(mhat,d,n)
 
 
 print(initPrimes)
 print('public key',e,n)
 print('private key',d)
-print('time', stop-start)
-# print('message', m)
+# print('time', stop-start)
+# print(decode == m)
+# print(m)
+# print(mhat)
+# print(decode)
